9cb684bdfbb22724a54519b4d688b0f8208300a8
Mauricio de Assis <mauriciox8mm@hotmail.com>
2020-08-27 21:44:57 -0300

    alteração no script

--- resources/views/estudante/index.blade.php
+++ resources/views/estudante/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 
-
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
 <div class="row">
     <!-- left column -->
     <div class="container-fluid">
@@ -14,62 +14,52 @@
                 </div>
                 <!-- /.card-header -->
                 <!-- form start -->
-                <form method="POST" action="{{route('possui.cpf')}}">
 
-                    <div class="card card-primary card-outline card-outline-tabs">
-                        <div class="card-header p-0 border-bottom-0">
-                            <ul class="nav nav-tabs" id="custom-tabs-four-tab" role="tablist">
-                                <li class="nav-item">
-                                    <a class="nav-link" id="custom-tabs-four-home-tab" data-toggle="pill"
-                                        href="#custom-tabs-four-home" role="tab" aria-controls="custom-tabs-four-home"
-                                        aria-selected="false">Dados Pessoais </a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link active" id="custom-tabs-four-profile-tab" data-toggle="pill"
-                                        href="#custom-tabs-four-profile" role="tab"
-                                        aria-controls="custom-tabs-four-profile" aria-selected="true">Profile</a>
-                                </li>
 
-                            </ul>
+
+                <form role="form" id="dadosPessoais" novalidate="novalidate">
+                    <div class="card-body " id="divDadosPessoais">
+
+                        <div class="callout callout-danger">
+                            <h5>Atenção </h5>
+
+                            <p><b> Nesta tela todos os campos são obrigatórios </b></p>
                         </div>
-                        <div class="card-body">
-                            <div class="tab-content" id="custom-tabs-four-tabContent">
-                                <div class="tab-pane fade" id="custom-tabs-four-home" role="tabpanel"
-                                    aria-labelledby="custom-tabs-four-home-tab">
-                                    <div class="callout callout-danger">
-                                        <h5>Atenção </h5>
+                        <div class="form-group">
+                            <label for="nome">Nome do Aluno</label>
+                            <input type="text" name="nomeAluno" class="form-control" id="nomeAluno"
+                                placeholder="Nome completo" required>
+                        </div>
+                        <div class="form-group">
+                            <label for="responsavel">Nome do Responsável </label>
+                            <input type="text" name="responsavel" class="form-control" id="responsavel"
+                                placeholder="Nome completo " required>
+                        </div>
+                        <div class="form-group">
+                            <label for="naturalidade">Naturalidade</label>
+                            <input type="text" name="naturalidade" class="form-control" id="naturalidade"
+                                placeholder="Paranavaí " required>
+                        </div>
+                        <div class="form-group">
+                            <label>Telefone de contato :</label>
 
-                                        <p><b> Nesta tela todos os campos são obrigatórios </b></p>
-                                    </div>
-                                    <div class="form-group">
-                                        <label for="nome">Nome do Aluno</label>
-                                        <input type="text" name="nomeAluno" class="form-control" id="nomeAluno"
-                                            placeholder="Nome completo" required>
-                                    </div>
-                                    <div class="form-group">
-                                        <label for="responsavel">Nome do Responsável </label>
-                                        <input type="text" name="responsavel" class="form-control" id="responsavel"
-                                            placeholder="Nome completo " required>
-                                    </div>
-                                    <div class="form-group">
-                                        <label for="naturalidade">Naturalidade</label>
-                                        <input type="text" name="naturalidade" class="form-control" id="naturalidade"
-                                            placeholder="Paranavaí " required>
-                                    </div>
-                                    <div class="form-group">
-                                        <label>Telefone de contato :</label>
+                            <div class="input-group">
+                                <div class="input-group-prepend">
+                                    <span class="input-group-text"><i class="fas fa-phone"></i></span>
+                                </div>
+                                <input type="text" class="form-control" name="telefone" id="telefone"
+                                    data-inputmask="&quot;mask&quot;: &quot;(99) 99999-9999&quot;" data-mask=""
+                                    inputmode="verbatim" im-insert="true" required>
+                            </div>
+                            <div class="card-footer">
+                                <button type="submit" class="btn btn-primary" id="btn-dadosPessoais"><i
+                                        class="fas fa-long-arrow-alt-right"></i>
+                                    Próximo</button>
+                            </div>
 
-                                        <div class="input-group">
-                                            <div class="input-group-prepend">
-                                                <span class="input-group-text"><i class="fas fa-phone"></i></span>
-                                            </div>
-                                            <input type="text" class="form-control" name="telefone" id="telefone"
-                                                data-inputmask="&quot;mask&quot;: &quot;(99) 99999-9999&quot;"
-                                                data-mask="" inputmode="verbatim" im-insert="true" required>
-                                        </div>
-                                        <!-- /.input group -->
-                                    </div>
-                                    {{--  <div class="form-group">
+                            <!-- /.input group -->
+                        </div>
+                        {{--  <div class="form-group">
 
                         <div class="form-group">
                             <label>Minimal</label>
@@ -85,97 +75,73 @@
                             </select>
                         </div>
                     </div>  --}}
-                                    <!-- /.card-body -->
+                        <!-- /.card-body -->
 
-                                </div>
-                                <div class="tab-pane fade active show" id="custom-tabs-four-profile" role="tabpanel"
-                                    aria-labelledby="custom-tabs-four-profile-tab">
-
-                                    <div class="card-body">
-                                        <div class="callout callout-danger">
-                                            <h5>Atenção </h5>
-
-                                            <p><b> Nesta tela todos os campos são obrigatórios </b></p>
-                                        </div>
-                                        <div class="form-group">
-                                            <label for="nome">Nome do Aluno</label>
-                                            <input type="text" name="nomeAluno" class="form-control" id="nomeAluno"
-                                                placeholder="Nome completo" required>
-                                        </div>
-                                        <div class="form-group">
-                                            <label for="responsavel">Nome do Responsável </label>
-                                            <input type="text" name="responsavel" class="form-control" id="responsavel"
-                                                placeholder="Nome completo " required>
-                                        </div>
-                                        <div class="form-group">
-                                            <label for="naturalidade">Naturalidade</label>
-                                            <input type="text" name="naturalidade" class="form-control"
-                                                id="naturalidade" placeholder="Paranavaí " required>
-                                        </div>
-                                        <div class="form-group">
-                                            <label>Telefone de contato :</label>
-
-                                            <div class="input-group">
-                                                <div class="input-group-prepend">
-                                                    <span class="input-group-text"><i class="fas fa-phone"></i></span>
-                                                </div>
-                                                <input type="text" class="form-control" name="telefone" id="telefone"
-                                                    data-inputmask="&quot;mask&quot;: &quot;(99) 99999-9999&quot;"
-                                                    data-mask="" inputmode="verbatim" im-insert="true" required>
-                                            </div>
-                                            <!-- /.input group -->
-                                        </div>
-                                        {{--  <div class="form-group">
-
-                            <div class="form-group">
-                                <label>Minimal</label>
-                                <select class="form-control select2 select2-hidden-accessible" style="width: 100%;"
-                                    data-select2-id="2" tabindex="-1" aria-hidden="false">
-                                    <option selected="selected" data-select2-id="3">Alabama</option>
-                                    <option>Alaska</option>
-                                    <option>California</option>
-                                    <option>Delaware</option>
-                                    <option>Tennessee</option>
-                                    <option>Texas</option>
-                                    <option>Washington</option>
-                                </select>
-                            </div>
-                        </div>  --}}
-                                        <!-- /.card-body -->
-
-                                    </div>
-                                </div>
-
-
-                            </div>
-                        </div>
-                        <!-- /.card -->
                     </div>
 
+                </form>
+                <form action="" method="POST">
                     @csrf
-                    <div class="card-footer">
-                        <button type="submit" class="btn btn-primary"><i class="fas fa-long-arrow-alt-right"></i>
-                            Próximo</button>
+                    <div class="card-body" id="cpf">
+                        <div class="card-footer">
+                            <button type="submit" class="btn btn-primary"><i class="fas fa-long-arrow-alt-right"></i>
+                                Próximo</button>
+                        </div>
                     </div>
                 </form>
+
             </div>
+
+
         </div>
-
-
     </div>
     <!-- /.card -->
 </div>
-<!--/.col (left) -->
-<!-- right column -->
-<div class="col-md-6">
 
-</div>
-<!--/.col (right) -->
-</div>
-<!-- /.row -->
-</div>
-</div>
 <!-- /.container-fluid -->
 </section>
+<script>
+    $(document).ready(function () {
+        $("#cpf").hide();
 
+  $.validator.setDefaults({
+    submitHandler: function () {
+        $("#divDadosPessoais").hide();
+        $("#cpf").show();
+    }
+  });
+
+  $('#dadosPessoais').validate({
+    rules: {
+     nomeAluno: {
+        required: true,
+      },
+      responsavel: {
+        required: true,
+
+      },
+      naturalidade: {
+        required: true
+      },
+      telefone: {
+        required: true
+      },
+
+
+    },
+
+    errorElement: 'span',
+    errorPlacement: function (error, element) {
+      error.addClass('invalid-feedback');
+      element.closest('.form-group').append(error);
+    },
+    highlight: function (element, errorClass, validClass) {
+      $(element).addClass('is-invalid');
+    },
+    unhighlight: function (element, errorClass, validClass) {
+      $(element).removeClass('is-invalid');
+    }
+  });
+});
+</script>
 @endsection
